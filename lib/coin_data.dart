6d2ac4c7cee8d235e3f7ccd136434e8c34cf4694
flappycatt <patryk.szindler@protonmail.com>
2021-04-14 09:24:07 +0200

    final

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+const String apiKey = '63BE7554-85B7-45E6-8F30-13B84B49FCAE';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +35,18 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getCoinData(String cryptoValue, String Currency) async {
+    var url =
+        Uri.https('rest.coinapi.io', '/v1/exchangerate/$cryptoValue/$Currency');
+    var response = await http.get(url, headers: {'X-CoinAPI-Key': apiKey});
+    if (response.statusCode == 200) {
+      var decodedData = convert.jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with request';
+    }
+  }
+}
